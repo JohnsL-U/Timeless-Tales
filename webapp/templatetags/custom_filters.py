bc7499d4b5029bbfc613202e83c4380915bb0a4a
JohnsL-U <uzun.cansel@gmail.com>
2023-05-06 22:52:04 +0300

    The new custom_filters.py file includes a function for extracting the first image from Post descriptions, which are now in HTML format due to the introduction of the Quill Editor rich text field. This function uses the beautifulsoup4 library to parse the HTML and extract the image. This change is necessary to ensure that the first image is displayed correctly

---
+++ webapp/templatetags/custom_filters.py
@@ -0,0 +1,11 @@
+from django import template
+from bs4 import BeautifulSoup
+
+register = template.Library()
+
+@register.filter
+def extract_first_image(html_content):
+    soup = BeautifulSoup(html_content, 'html.parser')
+    img = soup.find('img')
+
+    return img['src'] if img else ''
