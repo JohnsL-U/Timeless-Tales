ae3d9e049ab33d1c9f1d19d071fda5e2e4391642
JohnsL-U <uzun.cansel@gmail.com>
2023-05-08 19:40:31 +0300

    Made necessary changes to implement Quill Editor.

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,5 +5,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(('webapp.urls', 'webapp'), namespace='webapp')), 
+    path('', include(('webapp.urls', 'webapp'), namespace='webapp')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
