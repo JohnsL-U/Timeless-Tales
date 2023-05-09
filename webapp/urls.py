95c937bf3e34704a12e3c84ee210948729df0d88
JohnsL-U <uzun.cansel@gmail.com>
2023-05-10 01:17:08 +0300

    Included url paths for the new create a post and get api key views.

--- webapp/urls.py
+++ webapp/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('', views.welcome, name='welcome'),
     path('search/', views.post_search, name='post_search'),
     path('search/<str:search>/', views.post_search, name='post_search'),
+    path('create/', views.create_a_post, name='create_a_post'),
     path('post/<int:post_id>/', views.post_detail, name='post_detail'),
     path('add_comment/<int:post_id>/', views.add_comment, name='add_comment'),
     path('delete_comment/<int:comment_id>/', views.delete_comment, name='delete_comment'),
@@ -24,9 +25,10 @@ urlpatterns = [
     path('login/', LoginView.as_view(redirect_authenticated_user=True), name='login'),
     path('logout/', LogoutView.as_view(next_page='webapp:welcome'), name='logout'),
     path('password_change/', views.password_change, name='password_change'),
-    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset.html'), name='password_reset'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset.html', email_template_name='password_reset_email.html'), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
+    path('reset/complete/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
     path('user_agreement/', views.user_agreement, name='user_agreement'),
+    path('get_api_key/', views.get_api_key, name='get_api_key')
 ]
